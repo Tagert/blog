a28cee4cdc7658535b7b1b73e30b92f11658f6e3
Tagert <mini.andrius@gmail.com>
2025-01-05 15:53:26 +0200

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,25 @@
+import { SearchForm } from "@/components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query ?? "";
+
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect With Entrepreneurs
+        </h1>
+
+        <p className="sub-heading text-3xl !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competition
+        </p>
+
+        <SearchForm query={query} />
+      </section>
+    </>
+  );
+}
